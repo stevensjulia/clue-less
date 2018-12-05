dedb2c4de76741b263b73e452723a90546fd48f0
rpellet1 <41952169+rpellet1@users.noreply.github.com>
2018-12-04 23:41:46 -0600

    Update board.py

--- board.py
+++ board.py
@@ -5,9 +5,11 @@ class GameBoard:
     def __init__(self):
         return
     def instantiate GameBoard(self):
-        ##we need to create spaces to
-        space = Space()
-        readingroom = Room()
+        ##Im not sure if this is how we were planning to create the spaces but I was thinking of 
+        ##continuing to define all the rooms and spaces like this
+        newspace = Space( space_id = 1, occupied = False, adjacentSpaces = [10,2], type = None)
+        Library = Room( space_id = 2, occupied = False, adjacentSpaces = [1,3], type = Room)
+    
 
     def get_space(space_id):
         return space_id
