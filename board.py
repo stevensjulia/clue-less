0361d04f67639b72be02dcf850f1bc02b4fc17e7
rpellet1 <41952169+rpellet1@users.noreply.github.com>
2018-12-04 23:28:16 -0600

    Update board.py

--- board.py
+++ board.py
@@ -1,11 +1,13 @@
 ##I created this file to remind us it needs to exist but currently the methods arent accurate 
-
+from space import Space 
 class GameBoard:
     spaces = []
     def __init__(self):
         return
-    def get_adjacent_spaces(space_id):
-        return adjacentSpaces
+    def instantiate GameBoard(self):
+        ##create the number of spaces on our board and give them all a numeric so we can easilly get adjacent spaces 
+        ##from Space class
+        space = Space()
 
     def get_space(space_id):
         return space_id
