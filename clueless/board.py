d749e50bb2cc9e1e487f4eb13c2d595050970b28
rpellet1 <41952169+rpellet1@users.noreply.github.com>
2018-12-08 12:50:03 -0600

    Update board.py

--- clueless/board.py
+++ clueless/board.py
@@ -14,25 +14,39 @@ class Space:
     space_id = None
     occupied = False
     adjacent_spaces = []
+    suspects = None
     type = None
 
     def __init__(self, space_id, space_type, adjacent_spaces):
         self.space_id = space_id
         self.type = space_type
         self.adjacent_spaces = adjacent_spaces
-
+        
     def set_occupied(self):
         self.occupied = True
+    
+    def set_notoccupied(self):
+        self.occupied = False
 
     def get_adjacent_spaces(self):
         return self.adjacentSpaces
-
+class HomeSpace(Space):
+    """
+    This represents the starting space for the players which is the sam each game. starting square only has one occupant at a time.
+    """
+    def __init__(self, space_id, adjacent_spaces, suspects=None):
+        super(Space, self).__init__(space_id, adjacent_spaces, suspects)
+    
 
 class Room(Space):
     associated_card = None
     secret_passage = None
-
-    def __init__(self, space_id, space_type, associated_card, secret_passage=False):
+    weapons = None
+    def __init__(self, space_id, space_type, associated_card, weapons, secret_passage=False):
         Space.__init__(space_id, space_type)
         self.associated_card = associated_card
         self.secret_passage = secret_passage
+    if weapons:
+            self.weapons = weapons
+        else:
+            self.weapons = list()
